8db4f18decb7e589c1a0b6c9f948bd9169595a93
MoreNaruto <thomasmorris@tmorris.local>
2014-06-22 13:35:08 -0500

    Failed test for "Make a Move" created

--- test/BoardTest.java
+++ test/BoardTest.java
@@ -2,10 +2,13 @@ import com.company.Board;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by thomasmorris on 6/22/14.
@@ -13,11 +16,13 @@ import static org.mockito.Mockito.verify;
 public class BoardTest {
 
     private PrintStream mockStream;
+    private BufferedReader mockReader;
     private Board board;
 
     @Before
     public void setUp(){
         mockStream = mock(PrintStream.class);
+        mockReader = mock(BufferedReader.class);
         board = new Board(mockStream);
     }
     @Test
@@ -29,6 +34,18 @@ public class BoardTest {
                 "  |  |  \n" +
                 "--------\n" +
                 "  |  |  " );
+    }
+
+    @Test
+    public void testPlayer1MakeMove() throws IOException {
+        when(mockReader.readLine()).thenReturn("1");
+        verify(mockStream).println(
+                " X|  |  \n" +
+                "--------\n" +
+                "  |  |  \n" +
+                "--------\n" +
+                "  |  |  "
+        );
     }
 
 }
